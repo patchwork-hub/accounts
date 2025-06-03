d3fccad227a7c8225a41992a9aab9e6a19ea05c2
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-03 15:54:39 +0630

    feat:email

--- config/initializers/prepend_accounts.rb
+++ config/initializers/prepend_accounts.rb
@@ -4,4 +4,5 @@ Rails.application.config.to_prepare do
   Api::V1::AccountsController.prepend(Accounts::Concerns::AccountsCreation)
   Oauth::TokensController.prepend(Accounts::Concerns::CustomOauthBehavior)
   Account.include(Accounts::Concerns::AccountConcern)
+  REST::CredentialAccountSerializer.prepend(Overrides::CredentialAccountSerializer)
 end
