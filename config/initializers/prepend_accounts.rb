da73b0f16d1afcb7c15898aa5fa56d687cb8ea7c
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-11-08 13:41:29 +0630

    Implement custom authentication behavior for OAuth login handling

--- config/initializers/prepend_accounts.rb
+++ config/initializers/prepend_accounts.rb
@@ -2,8 +2,8 @@
 
 Rails.application.config.to_prepare do
   Api::V1::AccountsController.prepend(Accounts::Concerns::AccountsCreation)
-  Auth::TokensController.prepend(Accounts::Concerns::CustomOauthBehavior) if Object.const_defined?('Oauth::TokensController')
-  Oauth::TokensController.prepend(Accounts::Concerns::CustomOauthBehavior) if Object.const_defined?('Oauth::TokensController')
+  Auth::TokensController.prepend(Accounts::Concerns::CustomAuthenticationBehavior) if Object.const_defined?('Auth::TokensController')
+  OAuth::TokensController.prepend(Accounts::Concerns::CustomAuthenticationBehavior) if Object.const_defined?('OAuth::TokensController')
   Account.include(Accounts::Concerns::AccountConcern)
   # User.include(OverrideDeviseConfirmation)
   # User.include(OverrideDevisePassword)
