81f059b02fe676db06222558c96b3c5743e208a8
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2026-01-13 20:13:19 +0700

    Do not skip confirmation

--- app/services/overrides/app_sign_up_service_extension.rb
+++ app/services/overrides/app_sign_up_service_extension.rb
@@ -5,7 +5,7 @@ module Overrides::AppSignUpServiceExtension
     @user = User.create!(
       user_params.merge(created_by_application: @app, sign_up_ip: @remote_ip, password_confirmation: user_params[:password], account_attributes: account_params, invite_request_attributes: invite_request_params)
     )
-    @user.skip_confirmation!
+    # @user.skip_confirmation!
   end
 
   def invite_request_params
