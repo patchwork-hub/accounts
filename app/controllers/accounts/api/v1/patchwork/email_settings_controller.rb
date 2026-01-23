62856f39ff1c8bc180195da955655b1264769a37
NangKhatMang <50686926+NangKhatMang@users.noreply.github.com>
2026-01-23 13:22:18 +0630

    Merged fix: email notification setting update from api (#6)

--- app/controllers/accounts/api/v1/patchwork/email_settings_controller.rb
+++ app/controllers/accounts/api/v1/patchwork/email_settings_controller.rb
@@ -21,7 +21,7 @@ module Accounts::Api::V1::Patchwork
 
     def email_notification
       settings = enable_email_notification? ? email_notification_attributes(enabled: true) : email_notification_attributes(enabled: false)
-      if current_user.update(settings: settings)
+      if current_user.update(settings_attributes: settings)
         render_success({}, 'api.messages.success', :ok)
       else
         render_error('api.errors.unprocessable_entity', :unprocessable_entity)
