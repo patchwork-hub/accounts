bdf2d91ae1259063821dc474e156a8061a062994
NangKhatMang <nkm.nangkhatmang@gmail.com>
2026-01-26 16:56:21 +0630

    fix: email-notification and change ServerSetting

--- app/controllers/accounts/api/v1/patchwork/email_settings_controller.rb
+++ app/controllers/accounts/api/v1/patchwork/email_settings_controller.rb
@@ -3,6 +3,7 @@
 module Accounts::Api::V1::Patchwork
   class EmailSettingsController < Api::BaseController
     include Accounts::Concerns::ApiResponseHelper
+    include EmailNotificationAttributesConcern
 
     before_action -> { doorkeeper_authorize! :read, :write }
     before_action :require_user!
@@ -20,7 +21,7 @@ module Accounts::Api::V1::Patchwork
 
     def email_notification
       settings = enable_email_notification? ? email_notification_attributes(enabled: true) : email_notification_attributes(enabled: false)
-      if current_user.update(settings: settings)
+      if current_user.update(settings_attributes: settings)
         render_success({}, 'api.messages.success', :ok)
       else
         render_error('api.errors.unprocessable_entity', :unprocessable_entity)
@@ -39,22 +40,6 @@ module Accounts::Api::V1::Patchwork
 
     def truthy_param?(key)
       ActiveModel::Type::Boolean.new.cast(key)
-    end
-
-    def email_notification_attributes(enabled: false)
-      {
-        "always_send_emails" => enabled,
-        "notification_emails.follow" => enabled,
-        "notification_emails.reblog" => enabled,
-        "notification_emails.favourite" => enabled,
-        "notification_emails.mention" => enabled,
-        "notification_emails.follow_request" => enabled,
-        "notification_emails.report" => enabled,
-        "notification_emails.pending_account" => enabled,
-        "notification_emails.trends" => enabled,
-        "notification_emails.appeal" => enabled,
-        "notification_emails.software_updates" => enabled ? "critical" : "none"
-      }
     end
   end
 end
