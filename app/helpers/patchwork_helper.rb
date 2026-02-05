072d0e97feabdb9f124f386e80b06a838d6e9471
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-02-05 10:43:36 +0630

    update gems

--- app/helpers/patchwork_helper.rb
+++ app/helpers/patchwork_helper.rb
@@ -3,15 +3,27 @@
 module PatchworkHelper
   extend ActiveSupport::Concern
 
-  def patchwork_server_settings_exist?
-    return false unless Object.const_defined?('Accounts::ServerSetting') && defined?(Accounts::ServerSetting)
+  def patchwork_table_exists?(table_name)
+    ActiveRecord::Base.connection.data_source_exists?(table_name)
+  rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad
+    false
+  end
 
-    Accounts::ServerSetting.respond_to?(:table_exists?) && Accounts::ServerSetting.table_exists?
+  def patchwork_server_settings_exist?    
+    return false unless patchwork_table_exists?('server_settings')
+
+    return false unless Object.const_defined?('Accounts::ServerSetting') &&
+                        defined?(Accounts::ServerSetting) &&
+                        Accounts::ServerSetting.respond_to?(:find_by)
+
+    true
   end
 
   def patchwork_community_admin_exist?
-    return false unless Object.const_defined?('Accounts::CommunityAdmin') && defined?(Accounts::CommunityAdmin)
+    return false unless patchwork_table_exists?('patchwork_communities_admins')
 
-    Accounts::CommunityAdmin.respond_to?(:table_exists?) && Accounts::CommunityAdmin.table_exists?
+    return false unless Object.const_defined?('Accounts::CommunityAdmin') && defined?(Accounts::CommunityAdmin) && Accounts::CommunityAdmin.respond_to?(:find_by)
+    
+    true
   end
 end
