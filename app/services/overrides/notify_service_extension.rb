6ef235c9d88bde173f03e2dfe5977fbbf2635ba6
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2026-04-25 00:09:27 +0700

    temporarily comment out

--- app/services/overrides/notify_service_extension.rb
+++ app/services/overrides/notify_service_extension.rb
@@ -20,10 +20,10 @@ module Overrides::NotifyServiceExtension
     if @notification.filtered?
       update_notification_request!
       if @notification.type == :mention
-        mention = Mention.find(@notification.activity_id)
-        status = Status.find(mention.status_id)
+        # mention = Mention.find(@notification.activity_id)
+        # status = Status.find(mention.status_id)
         notification_request = NotificationRequest.find_by(account_id: @notification.account_id)
-        CustomNotificationService.new.call(@recipient, @notification) if notification_request.present? && notification_request.last_status_id == status.id
+        CustomNotificationService.new.call(@recipient, @notification) if notification_request.present? #&& notification_request.last_status_id == status.id
       end
     else
       push_notification!
