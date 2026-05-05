702b522efd5f32d5393c62ef6da6ef60f3237ca0
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-05-05 12:13:02 +0630

    Update account_concern.rb

--- app/models/accounts/concerns/account_concern.rb
+++ app/models/accounts/concerns/account_concern.rb
@@ -4,7 +4,7 @@ module Accounts::Concerns::AccountConcern
   extend ActiveSupport::Concern
 
   included do
-    has_many :notification_tokens, class_name: 'Accounts::NotificationToken', dependent: :delete_all, inverse_of: :account
+    has_many :notification_tokens, class_name: 'NotificationToken', dependent: :delete_all, inverse_of: :account
     has_many :patchwork_settings, class_name: 'Accounts::PatchworkSetting', foreign_key: :account_id, dependent: :destroy
   end
 end
